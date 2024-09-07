c91ec7aeaa784c5eeedfc36767508e62ac99b8b5
s1f102200659 <s1f102200659@iniad.org>
2024-09-08 00:42:20 +0900

    Initial settings

--- log_app/views.py
+++ log_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, 'kyoyu_app/login.html')
+
+def home(request):
+    return render(request, 'kyoyu_app/home.html')
+
+def make_kyoyusheet(request):
+    return render(request, 'kyoyu_app/make_kyoyusheet.html')
